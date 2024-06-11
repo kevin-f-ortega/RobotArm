81ecc94c12fd54ed4280560950e310a108c62833
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-06-11 13:46:29 -0700

    loading parameters

--- RobotArm/Top/RobotArmTopology.cpp
+++ RobotArm/Top/RobotArmTopology.cpp
@@ -154,7 +154,7 @@ void setupTopology(const TopologyState& state) {
     // Project-specific component configuration. Function provided above. May be inlined, if desired.
     configureTopology();
     // Autocoded parameter loading. Function provided by autocoder.
-    // loadParameters();
+    loadParameters();
     // Autocoded task kick-off (active components). Function provided by autocoder.
     startTasks(state);
     // Initialize socket communication if and only if there is a valid specification
